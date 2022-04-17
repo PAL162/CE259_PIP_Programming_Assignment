51896d8310998877ce84e8a49350dbc7d1bb5736
PAL162 <96531335+PAL162@users.noreply.github.com>
2022-04-17 22:22:22 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,14 @@
+try:
+    a = [1, 2, 3]
+print (a[3])except, LookupError:
+print ("Index out of bound error.")
+
+
+# import math
+# print(math.exp(1000))
+
+# array = [ 0, 1, 2 ]
+# print (array[3])
+
+
+#Pal_Khant_(D21CE162)
